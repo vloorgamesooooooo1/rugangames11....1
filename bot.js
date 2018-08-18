0e17580a085eda1d0a18ffb08dd0ca6d5429db6b
vloorgamesooooooo1 <42497492+vloorgamesooooooo1@users.noreply.github.com>
2018-08-18 22:29:56 +0200

    Update bot.js

--- bot.js
+++ bot.js
@@ -7,7 +7,7 @@ const GOOGLE_API_KEY = 'AIzaSyAdORXg7UZUo7sePv97JyoDqtQVi3Ll0b8';
 const youtube = new YouTube(GOOGLE_API_KEY);
 
 const queue = new Map();
-const PREFIX = '$';
+const PREFIX = '1';
 client.on('warn', console.warn);
 
 client.on('error', console.error);
@@ -314,7 +314,7 @@ client.on('message', async msg => { // eslint disable line
             },
             fields: [{
                 name: "آوامر البوت",
-                value: `**${PREFIX}help** - This message!\n\
+                value: `**${PREFIX}help** - معلومآت البوت ,\n\
 **${PREFIX}play** - لآختيار وتشغيل الآغاني المطلوبة\n\
 **${PREFIX}skip** - لتخطي الآغنية \n\
 **${PREFIX}stop** - لتغيير صوت البوت \n\
@@ -472,7 +472,7 @@ client.on('message', message => {
   if (!message.content.startsWith(PREFIX)) return;
   var args = message.content.split(' ').slice(1);
   var argresult = args.join(' ');
-  if (message.author.id !== "439187325503930369") return;
+  if (message.author.id !== "331081268731052042") return;
 
 if (message.content.startsWith(PREFIX + 'setstream')) {
   client.user.setGame(argresult, "https://www.twitch.tv/darkknite55");
